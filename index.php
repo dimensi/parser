93a0cfd9992f27847fa2a057ad5af17cf752f1f5
Nikita Nafranets <eddimensi@gmail.com>
2016-07-23 05:11:33 +0300

    Ничего

--- index.php
+++ index.php
@@ -7,13 +7,12 @@
     $document = getDocument('http://tiesshop.ru/catalog/102');
     $findItems = findItems($document);
     $pages = getPages($findItems);
-    print_r($pages);
-    // $printTableTitles = array_keys($pages[0]['content']);
-    // echo mb_convert_encoding("Название;","windows-1251");
-    // echo mb_convert_encoding(implode(";", $printTableTitles) . PHP_EOL, "windows-1251");
-    // foreach ($pages as $key => $page) {
-    //     $titlePage = $pages[$key]['title'];
-    //     echo mb_convert_encoding($titlePage . ';', "windows-1251");
-    //     echo mb_convert_encoding(implode(";", $page['content']), "windows-1251");
-    //     echo PHP_EOL;
-    // }
+    $printTableTitles = array_keys($pages[0]['content']);
+    echo mb_convert_encoding('Название;', 'windows-1251');
+    echo mb_convert_encoding(implode(';', $printTableTitles).PHP_EOL, 'windows-1251');
+    foreach ($pages as $key => $page) {
+        $titlePage = $pages[$key]['title'];
+        echo mb_convert_encoding($titlePage.';', 'windows-1251');
+        echo mb_convert_encoding(implode(';', $page['content']), 'windows-1251');
+        echo PHP_EOL;
+    }
