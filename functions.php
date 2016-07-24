2257ea07a7288a78f69072618de041307a2a7638
Nikita Nafranets <eddimensi@gmail.com>
2016-07-24 08:09:22 +0300

    Обновил функции и додумал парсер, обновил readme

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 namespace Functions;
 
 require_once 'phpQuery/phpQuery.php';
-function getDocument($link)
+function getDocument($link) // Получает файл и сохраняет его как новым дом
 {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $link);
@@ -14,7 +14,7 @@ function getDocument($link)
     return \phpQuery::newDocumentHTML($catalog);
 }
 
-function findItems($document)
+function findItems($document) // Получает из каталога все ссылки + проверяет на наличие
 {
     $links = $document->find('.good_img');
     foreach ($links as $value) {
@@ -29,33 +29,72 @@ function findItems($document)
     return $linksFromCatalog;
 }
 
-function getTitlePage($document)
+function getTitlePage($document) // Получает название со страницы товара
 {
-    return $document->find('.pad > h1')->text();
+    $title = $document->find('.pad > h1')->text();
+    $clearTitle = preg_replace('/\b[HSM]{1}[0-9]*\b|\!{2,}/', '', $title); // Очищаю название товара от мусора
+
+    return mb_convert_encoding(trim(preg_replace('/\s{2,}/', ' ', $clearTitle)), 'windows-1251'); // Очищаю от лишних пробелов и конвертирую
 }
 
-function getTablePage($document)
+function getSkuPage($title) // Получает SKU из названия товара
 {
-    foreach ($document->find('table#properties > tr') as $value) {
-        $trPq = pq($value);
-        $tdKey = $trPq->children('td')->eq(0)->text();
-        $tdContent = $trPq->children('td')->eq(1)->text();
-        $table[$tdKey] = $tdContent;
-    }
-
-    return $table;
+    return trim(preg_replace('/\D{0,}\b/', '', $title));
 }
 
-function getPages($pagesLinks)
+function getPages($links) // Собирает все данные из страниц и сохраняет в массив
 {
-    foreach ($pagesLinks as $itemLink) {
-        $itemDocument = getDocument($itemLink);
-        $title = getTitlePage($itemDocument);
-        $table = getTablePage($itemDocument);
-        $pageContent['title'] = $title;
-        $pageContent['content'] = $table;
-        $pages[] = $pageContent;
+    $nameTitle = mb_convert_encoding('Название', 'windows-1251'); // заготовка
+    foreach ($links as $link) {
+        $pageDoc = getDocument($link); // Получил дом
+        $title = getTitlePage($pageDoc); // Получил название
+        $sku = getSkuPage($title); // Получил SKU
+        $table[$nameTitle] = $title; // Записал название в массив
+        $table['SKU'] = $sku; // Записал SKU в массив
+        foreach ($pageDoc->find('table#properties > tr') as $tr) {
+            $trPq = pq($tr);
+            $tdKey = mb_convert_encoding(trim($trPq->children('td')->eq(0)->text()), 'windows-1251'); // Обрезаю и конвертирую
+            $tdContent = mb_convert_encoding(trim($trPq->children('td')->eq(1)->text()), 'windows-1251');
+            $table[$tdKey] = $tdContent;
+        } // Прошелся foreach по всем страницам и собрал нужные данные
+        $pages[] = $table; // Записал в единный массив
     }
 
-    return $pages;
+    return $pages; // Вернул единный массив
+}
+function createLexicon($pages) // Создает словарь со всеми названиями типов характеристик
+{
+    $lexicon = []; // Создал массив
+    foreach ($pages as $keys) {
+        foreach (array_keys($keys) as $key) {
+            if (!array_key_exists($key, $lexicon)) {
+                $lexicon[$key] = null; // Записываю новый ключ, если его нет в массиве
+            }
+        }
+    }
+
+    return $lexicon; // Возвращаю словарь
+}
+
+function updateArrays($pages, $lexicon) // Сверяю со словарем каждый массив
+{
+    foreach ($pages as $onePageKey => $onePage) { // Запускаю foreach для каждого массива
+        foreach ($lexicon as $lexKey => $lexValue) { // Проверяю foreach словарь и если нет похожего ключа, создаю новый с пустым значением
+            if (!array_key_exists($lexKey, $onePage)) {
+                $pages[$onePageKey][$lexKey] = null;
+            }
+        }
+    }
+
+    return $pages; // Возвращаю обработанный массив
+}
+
+function saveToCSV($array, $filename) // Сохраняю в csv
+{
+    $fp = fopen($filename, 'w');
+    fputcsv($fp, array_keys($array[0]), ';');
+    foreach ($array as $value) {
+        fputcsv($fp, $value, ';');
+    }
+    fclose($fp);
 }
--- index.php
+++ index.php
@@ -3,16 +3,13 @@
     use function \Functions\getDocument;
     use function \Functions\findItems;
     use function \Functions\getPages;
+    use function \Functions\createLexicon;
+    use function \Functions\updateArrays;
+    use function \Functions\saveToCSV;
 
     $document = getDocument('http://tiesshop.ru/catalog/102');
     $findItems = findItems($document);
     $pages = getPages($findItems);
-    $printTableTitles = array_keys($pages[0]['content']);
-    echo mb_convert_encoding('Название;', 'windows-1251');
-    echo mb_convert_encoding(implode(';', $printTableTitles).PHP_EOL, 'windows-1251');
-    foreach ($pages as $key => $page) {
-        $titlePage = $pages[$key]['title'];
-        echo mb_convert_encoding($titlePage.';', 'windows-1251');
-        echo mb_convert_encoding(implode(';', $page['content']), 'windows-1251');
-        echo PHP_EOL;
-    }
+    $lexicon = createLexicon($pages);
+    $result = updateArrays($pages, $lexicon);
+    saveToCSV($result, 'file.csv');
